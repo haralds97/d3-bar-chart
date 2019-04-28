63aeed18a9cbdf985ff92938ddf1ec6bc1de7545
haraldsDev <Haralds.Dev@gmail.com>
2019-04-28 21:48:33 +0300

    tooltips, overlay fixed + clean up of comments

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 
-// import { duplicateData, emotionsAtTime } from './rawData.js';
-// const { duplicateData, emotionsAtTime} = require('rawData.js');
-
-// var d3 = require("d3");
-// import d3 from "d3";
-
-// const projectName = 'bar-chart';
-// localStorage.setItem('example_project', 'D3: Bar Chart');
-// coded by @Christian-Paul 
-
-
 
 // declared 'width', 'height', and 'GDP barwidth'
 var yMargin = 40;
 var width = 800;
 var height = 400;
-var barWidth = width/19;
+var barWidth = width/18;
 
-// 'tooltip' is a small pop-up which shows when cursor or pointer goes over some object. The width, height and other properties of this 'tooltip' are defined in the style.css file. ('tooltip' is a random name here - could be named other ways). https://www.w3schools.com/howto/howto_css_tooltip.asp 
-// we are selecting a class '.visHolder' which is is index.html --> and appending 'div' with an 'id' of 'tooltip'
+
 var tooltip = d3.select(".visHolder").append("div")
   .attr("id", "tooltip")
   .style("opacity", 0);
@@ -39,8 +27,8 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
   // text at the left - created here
   svgContainer.append('text')
     .attr('transform', 'rotate(-90)')
-    .attr('x', -200)
-    .attr('y', 80)
+    .attr('x', -220)
+    .attr('y', 25)
     .text('Pulksteņa laiki');
   
   // text at the bottom - created here
@@ -50,70 +38,14 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     .text('More Information: no Mood-Tracker app')
     .attr('class', 'info');
   
-  // mapping years from API json. First 'data' refers to line 28 definition of all json file as 'data'. second 'data' refers to a 'key' by name 'data' in the json file.
-  // var years = data.data.map(item => {
-  //   var quarter;
-
-  //   // temporary variable to 'slice' some part of json API data
-  //   var temp = item[0].substring(5, 7);
-    
-  //   if(temp === '01') {
-  //     quarter = 'Q1';
-  //   }
-  //   else if (temp === '04'){
-  //     quarter = 'Q2';
-  //   }
-  //   else if(temp === '07') {
-  //     quarter = 'Q3';
-  //   }
-  //   else if(temp ==='10') {
-  //     quarter = 'Q4';
-  //   }
-
-  //   // second part of mapping function which results in 'var years'
-  //   return item[0].substring(0, 4) + ' ' + quarter
-  // });
-  
-
-  // mapping years from API json. First 'data' refers to line 28 definition of all json file as 'data'. second 'data' refers to a 'key' by name 'data' in the json file.
-  var yearsDate = data.data.map(function(item) {
-   
-   // a way to return a 'Date()' with specified date - taken from the array inside 'data', which looks like this - so we grap a 'year-month-date'
-  // "data": [
-  //   [
-  //     "1947-01-01",
-  //     243.1
-  //   ],
-    return new Date(item[0]);
-  });
-
-  // calling a 'max()' in-built function of d3, to find the MAX year&quarter from 'yearsDate'. First saving it in 'Date()' format. and then in a 'var xMax' - for setting maximum for 'x axis'.
-  // var xMax = new Date(d3.max(yearsDate));
-  var xMax = new Date(d3.max(sampleTicksForXaxis));
-
-  // javascript in-built methods 'setMonth' and 'getMonth'. First we 'getMonth' from 'var xMax' and add 3 months to it. Then we use this value to 'setMonth()' for xMax... ? - basically just adding 3 months to 'xMax'?
-  // xMax.setMonth(xMax.getMonth() + 3);
-
-  // 'scaleTime' is a d3 function. Basically we use minimum year and maximum year to set domain. and then map it to the range, and 'width' was set to '800' up in the code
-  
- 
-
   var xScale = d3.scaleTime()
     // .domain([d3.min(yearsDate), xMax])
-    .domain([d3.min(sampleTicksForXaxis), xMax])    
+    .domain([d3.min(sampleTicksForXaxis), d3.max(sampleTicksForXaxis)])    
     .range([0, width]);
 
-  // code snippet from stack overflow to get different time formats for 'x' axis
-  
-  // var scaleX = d3.scaleTime().range([0, width]);
-  // var axisBottom = d3.axisBottom(scaleX)
-  //              .ticks(d3.timeMinute, 10); // Every 10 minutes
-
-
-  // creating 'x Axis' with 'axisBottom()' method of d3
+    // creating 'x Axis' with 'axisBottom()' method of d3
   var xAxis = d3.axisBottom()
     .scale(xScale);
-    // .ticks(d3.timeMinute, 10);
   
     // creating horizontal line with 'g'
   var xAxisGroup = svgContainer.append('g')
@@ -121,21 +53,7 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     .attr('id', 'x-axis')
     // moving it 60px right on x axis, and 400px down on y axis
     .attr('transform', 'translate(60, 400)');
-  
 
-  // mapping over API json file, and returning the 'GDP' by quarters
-  var GDP = data.data.map(function(item) {
-    return item[1]
-  });
-  
-  // declaring empty array.. for.. what?
-  // var scaledGDP = [];
-  let scaleDayTimes = []
-  
-  // finding 'min' and 'max' of 'GDP' to find how high can be the table in vertical dimension
-  // pārvērst šo par min / max pulksteņa laiku par ko ir pieraksti
-  // var gdpMin = d3.min(GDP);
-  // var gdpMax = d3.max(GDP);
   let timeMin = d3.min(goodTimesToNumbers);
   let timeMax = d3.max(goodTimesToNumbers);
 
@@ -145,10 +63,8 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     .range([0, height]);
   
   // scaled GDP values are returned for displaying
-  scaleDayTimes = goodTimesToNumbers.map(function(item) {
-    return linearScale(item);
-  });
-  
+  let scaleDayTimes = goodTimesToNumbers.map(item => linearScale(item));
+    
   // getting values for 'y' axis
   var yAxisScale = d3.scaleLinear()
     .domain([timeMin, timeMax])
@@ -199,8 +115,8 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     // assigning value to every 'bar', assigning 'd' to 'height'
     .attr('width', barWidth)
     .attr('height', 40)
-    // filling rect with blue color
-    .style('fill', '#33adff')
+    // filling rect with green color
+    .style('fill', '#279b1a')
     .attr('transform', 'translate(60, 0)')
        
 
@@ -213,11 +129,11 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
       
       overlay.transition()
         .duration(0)
-        .style('height', d + 'px')
+        .style('height', 40 + 'px')
         .style('width', barWidth + 'px')
-        .style('opacity', .9)
-        .style('left', (i * barWidth) + 0 + 'px')
-        .style('top', height - d + 'px')
+        .style('opacity', .7)
+        .style('left', (i * barWidth) + 'px')
+        .style('top', height - d - 40  + 'px')
         .style('transform', 'translateX(60px)');
 
       tooltip.transition()
@@ -225,11 +141,10 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         .style('opacity', .9);
       
 
-      tooltip.html(sampleTicksForXaxis[i] + '<br>' + firstGoodEmotion[1] + '<br>' + secondGoodEmotion[2] +
-          '<br>' + thirdGoodEmotion[3])
+      tooltip.html(sampleTicksForXaxis[i] + '<br>' + arrayOfFirstGoodEmotion[i] + '<br>' + secondGoodEmotion[2] + '<br>' + thirdGoodEmotion[3])
         .attr('data-date', sampleTicksForXaxis[i])
-        .style('left', (i * barWidth) + 30 + 'px')
-        .style('top', height - 100 + 'px')
+        .style('left', (i * barWidth) + 50 + 'px')
+        .style('top', height - (d + 10) + 'px')
         .style('transform', 'translateX(60px)');
     })
     // styling on 'mouseout' event
@@ -247,7 +162,7 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
 });
 
 
-
+let arrayOfFirstGoodEmotion = ["draivs", "enerģija, gandarījums, kontakts", "enerģija, dominance, spēk", "", "", "mierīgs, ieinteresēts", "atslābums, nomierināšanās, atpūta", "gandarījums", "atslābums", "pacilātība", "varēšana", "miers", "miers", "pacilātība", "gaidas uz pusdienām", "enerģisks", "mundrums", "miers", "enerģija", "saulainums"];
 
 let goodTimes = ["18:00", "13:00", "14:00", "16:00", "18:00", "14:00", "17:00", "17:00", "12:00", "16:00", "13:00", "15:00", "15:00", "19:00", "9:00", "12:00", "14:00", "15:00"];
 
@@ -257,7 +172,8 @@ let sampleTicksForXaxis = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17, 18, 19];
 
 // duplicate data .... long file
 
-let duplicateData = [ { id: 135,
+let duplicateData = [ 
+  { id: 135,
     gulet: '',
     aizmig: '1:00',
     pamod: '10:30',
